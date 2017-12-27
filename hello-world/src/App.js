c78f58125d4b0f2324e670fbd5805327086b803a
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2017-12-27 16:10:42 +0530

    Added some react functionality

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -18,12 +18,48 @@ class App extends Component {
   }
 }
 class Form extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0,
+            username:'',
+            password:'',
+        };
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.updateMyScreen = this.updateMyScreen.bind(this);
+    }
+    handleUsername(event) {
+        this.setState({username: event.target.value});
+    }
+    handlePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+
+    updateMyScreen(){
+        event.preventDefault();
+        var count = this.state.count;
+        if(this.state.username==='user'){
+            alert('Logged in');
+            count++;
+        }
+        else{
+            alert("Invalid details");
+        }
+    }
+
     render(){
         return(
-        <form action="" method="">
-            <input name="username" type="text" placeholder="username"  />
-                <input type="password" placeholder="password" />
-            <center> <button className="btn btn-info btn-block login" type="submit">Login</button></center>
+        <form onSubmit={this.updateMyScreen}>
+            <input value={this.state.username} type="text" placeholder="username" onChange={this.handleUsername}  />
+                <input type="password" value={this.state.password} placeholder="password" onchange={this.handlePassword} />
+            <center>
+                <button className="btn btn-info btn-block login" type="submit">
+                People Online = {this.state.count}
+                </button>
+            </center>
         </form>
         );
     }
