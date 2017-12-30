a61dbee34d58d5b069f5eec7730bf6a9c8935a8d
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2017-12-30 22:37:09 +0530

    Added Validation

--- hello-world/src/Components/Forms.js
+++ hello-world/src/Components/Forms.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react';
 import '../App.css'
 import ReactDOM from 'react-dom';
+import {FormErrors} from './FormErrors';
+import './Form.css';
 class Form extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            count: 0,
-            username:'',
             password:'',
             sapID: '',
-            emailID: '',
+            email: '',
             isRegister: false,
             firstName:'',
             lastName:'',
             mobileNumber:'',
+            formErrors: {email: '', password: ''},
+            emailValid: false,
+            passwordValid: false,
+            phoneNumberValid: false,
+            formValid: false,
+            sapIDValid: false,
         };
-        this.handleUsername = this.handleUsername.bind(this);
+
+        this.handleUserInput = this.handleUserInput.bind(this);
         this.handlefirstName = this.handlefirstName.bind(this);
         this.handlelastName = this.handlelastName.bind(this);
         this.handlemobileNumber = this.handlemobileNumber.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
         this.handleSAP= this.handleSAP.bind(this);
         this.handleEmail= this.handleEmail.bind(this);
-        this.updateMyScreen = this.updateMyScreen.bind(this);
         this.addRegister = this.addRegister.bind(this);
     }
-    handleUsername(event) {
-        this.setState({username: event.target.value});
-    }
+
     handleSAP(event) {
         this.setState({sapID: event.target.value});
     }
     handleEmail(event) {
-        this.setState({Email: event.target.value});
+        this.setState({email: event.target.value});
     }
     handlePassword(event) {
         this.setState({password: event.target.value});
@@ -47,20 +51,60 @@ class Form extends Component{
     handlemobileNumber(event) {
         this.setState({mobileNumber: event.target.value});
     }
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let emailValid = this.state.emailValid;
+        let phoneNumberValid = this.state.phoneNumberValid;
+        let passwordValid = this.state.passwordValid;
+        let sapIDValid = this.state.sapIDValid;
 
+        switch(fieldName) {
+            case 'email':
+                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+                break;
+            case 'password':
+                passwordValid = value.length >= 6;
+                fieldValidationErrors.password = passwordValid ? '': ' is too short';
+                break;
+            case 'mobile':
+                if(this.state.isRegister){
+                    phoneNumberValid = value.length >= 10;
+                    fieldValidationErrors.mobileNumber = phoneNumberValid ? '': ' is too short';
+                }
+                break;
+            case  'sapID':
+                if(this.state.isRegister){
+                    sapIDValid = value.length >= 10;
+                    fieldValidationErrors.sapIDValid = sapIDValid ? '' : 'is invalid';
+                }
+                break;
 
-    updateMyScreen() {
-        event.preventDefault();
-        if(this.state.isRegister) {
-
+            default:
+                break;
         }
-        else{
-            if (this.state.username === '' || this.state.password === '') {
-                alert("Do not leave fields blank");
-            }
-        }
+        this.setState({formErrors: fieldValidationErrors,
+            emailValid: emailValid,
+            passwordValid: passwordValid
+        }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    }
+
+    errorClass(error) {
+        return(error.length === 0 ? '' : 'has-error');
+    }
 
 
+
+
+    handleUserInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value},
+            () => { this.validateField(name, value) });
     }
     addRegister(){
         event.preventDefault();
@@ -78,7 +122,6 @@ class Form extends Component{
                 <input type="text" placeholder="First Name" value={this.state.firstName} onChange={this.handlefirstName} required/>
                 <input type="text" placeholder="Last Name" value={this.state.lastName} onChange={this.handlelastName} required/>
                 <input type="text" placeholder="SAP ID" value={this.state.sapID} onChange={this.handleSAP} required/>
-                <input type="text" placeholder="Email Address" value={this.state.Email} onChange={this.handleEmail} required/>
                 <input type="text" placeholder="Mobile Number" value={this.state.mobileNumber} onChange={this.handlemobileNumber} required/>
 
             </div>
@@ -86,17 +129,24 @@ class Form extends Component{
         return(
             <div>
                 <form>
+                  <div className="panel panel-default">
+                      <FormErrors formErrors={this.state.formErrors} />
+                  </div>
                     {button}
-                    <input value={this.state.username} type="text" placeholder="Username" onChange={this.handleUsername} required />
-                    <input type="password" value={this.state.password} placeholder="password" onChange={this.handlePassword} required />
+                    <div className={`form-group ${this.errorClass(this.state.formErrors.email)}`}>
+                    <input  name="email" value={this.state.email} type="email" placeholder="Email" onChange={this.handleUserInput} required />
+                    </div>
+                    <div className={`form-group ${this.errorClass(this.state.formErrors.password)}`}>
+                    <input name="password" type="password" value={this.state.password} placeholder="Password" onChange={this.handleUserInput} required />
+                    </div>
                     <center>
-                        <button className="btn btn-info btn-block login" style={{textAlign: 'center', width: '100%'}} type="submit" onClick={this.updateMyScreen}>
+                        <button className="btn btn-info btn-block login" style={{textAlign: 'center', width: '100%'}} type="submit" disabled={!this.state.formValid} >
                             Login
                         </button>
                         </center>
                 </form>
                 <center>
-                    <button id="register" className="btn btn-info btn-block login" style={{width:'100%', backgroundColor: '#e7998f'}} onClick={this.addRegister}>
+                    <button id="register" className="btn btn-info btn-block login" style={{width:'100%', backgroundColor: '#e7998f'}} onClick={this.addRegister} disabled={this.state.isRegister}>
                         Register
                         </button>
                 </center>
