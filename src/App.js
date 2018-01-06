51d83667ce22d8de4668c74551dd6860d3944ed3
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2018-01-06 22:41:19 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+class App extends Component {
+  render() {
+    return (
+     <div>
+         <Header />
+
+         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                 <br/><br/><br/>
+         <Footer />
+     </div>
+    );
+  }
+}
+
+export default App;
