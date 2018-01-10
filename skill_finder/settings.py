03326cbfe0fafaea7040ec9e90a1932755440ba9
Parth Doshi <studentparth18@gmail.com>
2018-01-10 16:12:19 +0530

    Added the users app to the settings file.

--- skill_finder/settings.py
+++ skill_finder/settings.py
@@ -19,6 +19,7 @@ DEBUG = os.environ['DEBUG']
 ALLOWED_HOSTS = []
 # Application definition
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
