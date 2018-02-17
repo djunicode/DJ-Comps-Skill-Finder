83ec375d805168da205b0fbb525c5fbb2805b1b1
Devansh Dalal <devanshdalal98@gmail.com>
2018-02-17 13:58:37 +0530

    Profile display page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import '../App.css';
 class Header extends Component {
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <div className="demo-layout-transparent mdl-layout mdl-js-layout mdl-layout--fixed-header Ellipse">
         <div className="page-header mdl-layout__header mdl-layout__header--waterfall">
           <div className="mdl-layout__header-row">
             <span className="page-title mdl-layout-title">
-              <img className="page-logo-image" src="../UNCLetters.png" />
+              <img className="page-logo-image" src="../UNCLetters.png" alt="page-logo" />
             </span>
             <div className="page-header-spacer mdl-layout-spacer"></div>
             <div className="page-search-box mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right mdl-textfield--full-width">
@@ -20,13 +20,15 @@ class Header extends Component {
             </div>
             <div className="page-navigation-container">
               <nav className="page-navigation mdl-navigation">
-                <a className="mdl-navigation__link mdl-typography--text-uppercase" href="">PlaceHolder</a>
-                <a className="mdl-navigation__link mdl-typography--text-uppercase" href="">PlaceHolder</a>
-                <a className="mdl-navigation__link mdl-typography--text-uppercase" href="">PlaceHolder</a>
+                <a className="mdl-navigation__link" href="">Home</a>
+                <a className="mdl-navigation__link" href="">Find a Mentor</a>
+                <a className="mdl-navigation__link" href="">Make a Team</a>
+                <a className="mdl-navigation__link" href="">Events</a>
+                <a className="mdl-navigation__link" href="">Snoopy<i className="material-icons">person</i></a>
               </nav>
             </div>
             <span className="page-mobile-title mdl-layout-title">
-              <img className="page-logo-image" src="../UNCLetters.png" />
+              <img className="page-logo-image" src="../UNCLetters.png" alt="page-logo"/>
             </span>
 
             <button className="page-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
@@ -38,12 +40,12 @@ class Header extends Component {
               <li disabled className="mdl-menu__item">PlaceHolder</li>
               <li className="mdl-menu__item">PlaceHolder</li>
             </ul>
-
           </div>
+         
         </div>
         <div className="page-drawer mdl-layout__drawer">
           <span className="mdl-layout-title">
-            <img className="page-logo-image" src="../UNCLetters.png" />
+            <img className="page-logo-image" src="../UNCLetters.png" alt="page-logo" />
           </span>
           <nav className="mdl-navigation">
             <a className="mdl-navigation__link" href="">PlaceHolder 1</a>
@@ -68,6 +70,17 @@ class Header extends Component {
             <a className="mdl-navigation__link" href="">PlaceHolder</a>
           </nav>
         </div>
+        <br/><br/>
+        <img src="profile.png" className="img-circle" alt="User Profile Picture"/>
+          <div className="description">
+            <h2>Snoopy</h2>
+            <h6>"I love coding and cat videos"</h6>
+          </div>
+          <br/>
+          <button className="follow-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+            FOLLOW
+          </button>
+          <br/><br/>
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css';
+class Main extends Component {
+    render() {
+        return (
+        	<div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        		<h1>hi</h1>
+        	</div>
+       	);
+    }
+}
+
+export default Main;
