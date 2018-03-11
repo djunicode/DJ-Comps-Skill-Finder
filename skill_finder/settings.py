4cd36fb3291387882b88947bbc19c75a13c784a1
Devansh Dalal <devanshdalal98@gmail.com>
2018-03-11 20:12:52 +0530

    Fixed image size in the cards and added a dropdown for choosing a skill

--- skill_finder/settings.py
+++ skill_finder/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "nad7kycy!op3$^p@37=xk!mcwmmv5zw6@*e$r+#m5ood2)@i78"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,14 +82,9 @@ WSGI_APPLICATION = 'skill_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ['ENGINE'],
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_HOST'],
-        'DB_PORT': os.environ['DB_PORT'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
 
 # Password validation
