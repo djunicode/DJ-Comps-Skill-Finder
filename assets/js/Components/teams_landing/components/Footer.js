34035f9a94dcbb89030ed1d6c083952c89d241a3
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2018-09-17 22:53:05 +0530

    Removed placeholders from footer

--- assets/js/Components/teams_landing/components/Footer.js
+++ assets/js/Components/teams_landing/components/Footer.js
@@ -19,33 +19,12 @@ class Footer extends Component {
                     </div>
 
                     <div className="mdl-mega-footer--middle-section">
-                        <p className="mdl-typography--font-light">Some text would go here</p>
-                        <p className="mdl-typography--font-light">Some text can go here too</p>
+
                     </div>
 
                     <div className="mdl-mega-footer--bottom-section">
-                        <a className="page-link page-link-menu mdl-typography--font-light" id="version-dropdown">
-                            PlaceHolder
-                            <i className="material-icons">arrow_drop_up</i>
-                        </a>
-                        <ul className="mdl-menu mdl-js-menu mdl-menu--top-left mdl-js-ripple-effect" htmlFor="version-dropdown">
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                        </ul>
-                        <a className="page-link page-link-menu mdl-typography--font-light" id="developers-dropdown">
-                            PlaceHolder
-                            <i className="material-icons">arrow_drop_up</i>
-                        </a>
-                        <ul className="mdl-menu mdl-js-menu mdl-menu--top-left mdl-js-ripple-effect" htmlFor="developers-dropdown">
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                            <li className="mdl-menu__item">PlaceHolder</li>
-                        </ul>
-                        <a className="page-link mdl-typography--font-light" href="">PlaceHolder</a>
-                        <a className="page-link mdl-typography--font-light" href="">PlaceHolder</a>
+
+                        
                     </div>
 
 
